6e683f7dd41f623a6265e530c06645c84ac48368
liyang.zhang <liyang.zhang@finmechanics.com>
2017-05-16 08:58:55 +0800

    +combination sum

--- src/com/Yuzu/Solution.java
+++ src/com/Yuzu/Solution.java
@@ -1,8 +1,8 @@
 package com.Yuzu;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Liyang, Zhang on 5/12/2017.
@@ -19,7 +19,7 @@ public class Solution {
         int rightValue = nums[right];
         if (leftValue > target || rightValue < target) return notFound;
         while (left < right) {
-            int mid = (left + right)/2;
+            int mid = (left + right) / 2;
             int midValue = nums[mid];
             if (midValue < target) left = mid + 1;
             else if (midValue > target) right = mid;
@@ -36,7 +36,7 @@ public class Solution {
     private int findRight(int[] nums, int left, int right, int target) {
         if (nums[right] == target) return right;
         while (left < right) {
-            int mid = (left + right)/2 + 1;
+            int mid = (left + right) / 2 + 1;
             int midValue = nums[mid];
             if (midValue > target) right = mid - 1;
             else left = mid;
@@ -47,7 +47,7 @@ public class Solution {
     private int findLeft(int[] nums, int left, int right, int target) {
         if (nums[left] == target) return left;
         while (left < right) {
-            int mid = (left + right)/2;
+            int mid = (left + right) / 2;
             int midValue = nums[mid];
             if (midValue < target) left = mid + 1;
             else right = mid;
@@ -68,16 +68,16 @@ public class Solution {
                     stackOp.push(dig);
                     stackCount.push(0);
                 } else {
-                    postFix+=dig;
-                    stackCount.push((int)stackCount.pop() + 1);
+                    postFix += dig;
+                    stackCount.push((int) stackCount.pop() + 1);
                 }
-                if ((int)stackCount.peek() == 2) {
-                    postFix+=stackOp.pop();
+                if ((int) stackCount.peek() == 2) {
+                    postFix += stackOp.pop();
                     stackCount.pop();
-                    stackCount.push((int)stackCount.pop() + 1);
+                    stackCount.push((int) stackCount.pop() + 1);
                 }
             }
-            while (!stackOp.empty()) postFix+=stackOp.pop();
+            while (!stackOp.empty()) postFix += stackOp.pop();
             result[i] = postFix;
         }
         return result;
@@ -90,10 +90,11 @@ public class Solution {
     private boolean solveOneBox(int i, int j, char[][] board) {
         final int RADIX = 10;
         boolean valid = false;
+        char boxValue = board[i][j];
         if (i != 8 || j != 8) {
-            int nextI = j==8?i+1:i;
-            int nextJ = j==8?0:j+1;
-            if (board[i][j] == '.') {
+            int nextI = j == 8 ? i + 1 : i;
+            int nextJ = j == 8 ? 0 : j + 1;
+            if (boxValue == '.') {
                 for (int v = 1; v < 10; v++) {
                     board[i][j] = Character.forDigit(v, RADIX);
                     if (!isBoxValueValid(i, j, board)) continue;
@@ -102,12 +103,14 @@ public class Solution {
                         break;
                     }
                 }
-            } else if (!isBoxValueValid(i, j, board)) return false;
-            else return solveOneBox(nextI, nextJ, board);
-        } else if (board[i][j] == '.') {
-                for (int v = 1; v < 10; v++)
-                    if (isBoxValueValid(i, j, board)) return true;
-            } else return isBoxValueValid(i, j, board);
+            } else return solveOneBox(nextI, nextJ, board);
+        } else if (boxValue == '.') {
+            for (int v = 1; v < 10; v++) {
+                board[i][j] = Character.forDigit(v, RADIX);
+                if (isBoxValueValid(i, j, board)) return true;
+            }
+        } else return isBoxValueValid(i, j, board);
+        if (!valid) board[i][j] = boxValue;
         return valid;
     }
 
@@ -117,16 +120,18 @@ public class Solution {
         Set set3 = new HashSet();
         for (int p = 0; p < 9; p++) {
             char value = board[i][p];
-            if (value == '.') continue;
-            if (set.contains(value)) return false;
-            set.add(value);
+            if (value != '.') {
+                if (set.contains(value))
+                    return false;
+                set.add(value);
+            }
             char value2 = board[p][j];
             if (value2 == '.') continue;
             if (set2.contains(value2)) return false;
             set2.add(value2);
         }
-        for (int p = i/3*3; p < i/3*3+3; p++) {
-            for (int q = j/3*3; q < i/3*3+3; q++) {
+        for (int p = i / 3 * 3; p < i / 3 * 3 + 3; p++) {
+            for (int q = j / 3 * 3; q < j / 3 * 3 + 3; q++) {
                 char value = board[p][q];
                 if (value == '.') continue;
                 if (set3.contains(value)) return false;
@@ -143,8 +148,8 @@ public class Solution {
         for (int j = 0; j < 9; j++)
             if (!checkValidity(0, 9, j, j + 1, board))
                 return false;
-        for (int i = 0; i < 7; i+=3)
-            for (int j = 0; j < 7; j+=3)
+        for (int i = 0; i < 7; i += 3)
+            for (int j = 0; j < 7; j += 3)
                 if (!checkValidity(i, i + 3, j, j + 3, board))
                     return false;
         return true;
@@ -167,7 +172,7 @@ public class Solution {
         Set set;
         Set set1;
         Set set2;
-        for (int i = 0;i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             set = new HashSet();
             for (int j = 0; j < 9; j++) {
                 char value = board[i][j];
@@ -176,7 +181,7 @@ public class Solution {
                 set.add(value);
             }
         }
-        for (int j = 0;j < 9; j++) {
+        for (int j = 0; j < 9; j++) {
             set = new HashSet();
             for (int i = 0; i < 9; i++) {
                 char value = board[i][j];
@@ -188,7 +193,7 @@ public class Solution {
         set = new HashSet();
         set1 = new HashSet();
         set2 = new HashSet();
-        for (int i = 0;i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 char value = board[i][j];
                 if (value == '.') continue;
@@ -211,7 +216,7 @@ public class Solution {
         set = new HashSet();
         set1 = new HashSet();
         set2 = new HashSet();
-        for (int i = 3;i < 6; i++) {
+        for (int i = 3; i < 6; i++) {
             for (int j = 0; j < 3; j++) {
                 char value = board[i][j];
                 if (value == '.') continue;
@@ -234,7 +239,7 @@ public class Solution {
         set = new HashSet();
         set1 = new HashSet();
         set2 = new HashSet();
-        for (int i = 6;i < 9; i++) {
+        for (int i = 6; i < 9; i++) {
             for (int j = 0; j < 3; j++) {
                 char value = board[i][j];
                 if (value == '.') continue;
@@ -255,6 +260,28 @@ public class Solution {
             }
         }
         return true;
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Map<Integer, List<List<Integer>>> result = new HashMap<>();
+        result.put(0, Stream.of(new ArrayList<Integer>()).collect(Collectors.toList()));
+        result.put(target, new ArrayList<>());
+        for (int candidate: candidates) {
+            for (int i = candidate; i <= target; i++) {
+                List<List<Integer>> resultP = result.get(i - candidate);
+                List<List<Integer>> resultI = result.containsKey(i)?result.get(i):new ArrayList<>();
+                if (resultP != null && resultP.size() > 0) {
+                    for (List<Integer> list: resultP) {
+                        List<Integer> listI = new ArrayList<>();
+                        listI.addAll(list);
+                        listI.add(candidate);
+                        resultI.add(listI);
+                    }
+                }
+                if (!result.containsKey(i)) result.put(i, resultI);
+            }
+        }
+        return result.get(target);
     }
 
 }
